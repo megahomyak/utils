b5216a835f7b9ce37bc409a447e2f149397fe2b2
megahomyak <g.megahomyak@gmail.com>
2023-01-02 21:40:42 +0500

    Made the derpificator ignore punctuation

--- word_derpificator.py
+++ word_derpificator.py
@@ -1,7 +1,7 @@
 import re
 
 print(re.sub(
-    r"[aieouy]+[^aieouy\s]+",
+    r"[aieouy]+[^aieouy\s\W]+",
     lambda m: m.group(0)[::-1],
     input("Enter a nickname: "),
     flags=re.IGNORECASE,
