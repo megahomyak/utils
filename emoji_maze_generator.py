80e58a82b8ff473c728d3dbb209b8d0e1fc74fa3
megahomyak <g.megahomyak@gmail.com>
2023-01-17 17:07:56 +0500

    Added a way to generate compact mazes

--- emoji_maze_generator.py
+++ emoji_maze_generator.py
@@ -4,7 +4,6 @@ EMPTY_EMOJI = ":popgoes2:"
 
 
 for combination, emoji in (
-    # Must be the all 16 combinations for full coverage (there are 15, though: the all-False one is not included)
     # top, bottom, left, right
     ((True, True, True, True), ":phiCross:"),
 
@@ -25,12 +24,21 @@ for combination, emoji in (
     ((False, True, False, False), ":phiTopNub:"),
     ((False, False, True, False), ":phiRightNub:"),
     ((False, False, False, True), ":phiLeftNub:"),
+
+    ((False, False, False, False), Exception("Walls with no neighbors are missing in the Phi emoji pack!"))
 ):
     if combination in emojis:
         raise Exception(f"The {combination} combination is mapped already!")
     emojis[combination] = emoji
+if len(emojis) != 16:
+    raise Exception("Please, provide all 16 possible combinations.")
 
 
+print("Would you like your emoji maze to be [compact] or [spacious]?")
+maze_type = input("Type in the [maze type] here: ")
+if maze_type not in ("compact", "spacious"):
+    print(f"Error: the maze type you provided (\"{maze_type}\") is not \"compact\" or \"spacious\".")
+    exit(1)
 print("Type in the maze. Every number sign (#) represents a wall, every space ( ) represents a pathway. End with an empty line.")
 lines = []
 while True:
@@ -53,13 +61,23 @@ def is_a_wall(x, y):
 for y in range(len(lines)):
     row = lines[y]
     for x in range(len(row)):
-        emojis_maze[y][x] = emojis[(
-            is_a_wall(x, y - 1),
-            is_a_wall(x, y + 1),
-            is_a_wall(x - 1, y),
-            is_a_wall(x + 1, y)
-        )] if is_a_wall(x, y) else EMPTY_EMOJI
+        if is_a_wall(x, y):
+            emoji = emojis[(
+                is_a_wall(x, y - 1),
+                is_a_wall(x, y + 1),
+                is_a_wall(x - 1, y),
+                is_a_wall(x + 1, y)
+            )]
+            if isinstance(emoji, Exception):
+                raise emoji
+        else:
+            emoji = EMPTY_EMOJI
+        emojis_maze[y][x] = emoji
 
 
-for row in emojis_maze:
-    print("".join(row))
+if maze_type == "compact":
+    for row in emojis_maze[::2]:
+        print("".join(row[::2]))
+elif maze_type == "spacious":
+    for row in emojis_maze:
+        print("".join(row))
