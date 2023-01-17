5609469d2ffb9176fd2b27e62208f180444d8c8b
megahomyak <g.megahomyak@gmail.com>
2023-01-17 11:48:09 +0500

    Added russian vowels

--- word_derpificator.py
+++ word_derpificator.py
@@ -1,6 +1,8 @@
 import re
 
-VOWELS = "aieouy"
+RUSSIAN_VOWELS = "уеыаоэяиюё"
+ENGLISH_VOWELS = "aieouy"
+VOWELS = RUSSIAN_VOWELS + ENGLISH_VOWELS
 print(re.sub(
     rf"[{VOWELS}]+[^{VOWELS}_\s\W]+",
     lambda m: m.group(0)[::-1],
