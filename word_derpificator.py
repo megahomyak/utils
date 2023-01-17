5248b5bea5689ce5656f5e7f8a4859a014eae114
megahomyak <g.megahomyak@gmail.com>
2023-01-17 09:15:36 +0500

    Underscores should not count

--- word_derpificator.py
+++ word_derpificator.py
@@ -1,7 +1,7 @@
 import re
 
 print(re.sub(
-    r"[aieouy]+[^aieouy\s\W]+",
+    r"[aieouy]+[^aieouy_\s\W]+",
     lambda m: m.group(0)[::-1],
     input("Enter the words: "),
     flags=re.IGNORECASE,
