c20c84278c55e8d14e3089c2be180e7e5c7bc4bc
megahomyak <g.megahomyak@gmail.com>
2023-01-24 23:05:40 +0500

    Color maze generator finished

--- color_maze_generator/src/main.rs
+++ color_maze_generator/src/main.rs
@@ -10,13 +10,26 @@ const MAZE_WIDTH: usize = 10;
 const MAZE_HEIGHT: usize = 10;
 const BEGINNING_MARK: &'static str = ":HandPointDown:";
 const END_MARK: &'static str = ":HandPointRight:";
+const EMPTY_SPOT: &'static str = ":popgoes2:";
+
+fn join<T: AsRef<str>, I: Iterator<Item = T>>(mut i: I, separator: &str) -> String {
+    let mut s = String::new();
+    if let Some(contents) = i.next() {
+        s.push_str(contents.as_ref());
+        for contents in i {
+            s.push_str(separator);
+            s.push_str(contents.as_ref());
+        }
+    }
+    s
+}
 
 fn get_mark_index(distance: Distance) -> usize {
     usize::try_from(distance.0 % u32::try_from(MARKS.len()).unwrap()).unwrap()
 }
 
-fn get_mark(distance: Distance) -> &'static str {
-    MARKS[get_mark_index(distance)]
+fn get_next_mark_index(mark_index: MarkIndex) -> MarkIndex {
+    (mark_index + 1) % MARKS.len()
 }
 
 #[derive(Clone, Debug)]
@@ -24,7 +37,8 @@ struct Matrix<const WIDTH: usize, const HEIGHT: usize, T> {
     contents: [[T; WIDTH]; HEIGHT],
 }
 
-type Maze = Matrix<MAZE_WIDTH, MAZE_HEIGHT, Option<Distance>>;
+type MarkIndex = usize;
+type Maze = Matrix<MAZE_WIDTH, MAZE_HEIGHT, Option<MarkIndex>>;
 
 impl<const WIDTH: usize, const HEIGHT: usize, T> Matrix<WIDTH, HEIGHT, T> {
     pub fn new(filler: T) -> Self
@@ -66,7 +80,7 @@ fn adjacent(
     maze: &Maze,
     x: usize,
     y: usize,
-) -> impl Iterator<Item = (Position, &Option<Distance>)> {
+) -> impl Iterator<Item = (Position, &Option<MarkIndex>)> {
     [(-1, 0), (1, 0), (0, 1), (0, -1)]
         .into_iter()
         .filter_map(move |(x_bias, y_bias)| {
@@ -84,16 +98,20 @@ struct State {
 }
 
 fn print_the_maze(maze: &Maze) {
-    for line in maze.contents {
-        for cell in line {
-            print!(
-                "{}",
-                cell.map(|distance| get_mark(distance))
-                    .unwrap_or(":popgoes2:")
-            )
-        }
-        print!("\n");
-    }
+    print!(
+        "{}",
+        join(
+            maze.contents.iter().map(|row| {
+                row.iter()
+                    .map(|cell| {
+                        cell.map(|mark_index| MARKS[mark_index])
+                            .unwrap_or(EMPTY_SPOT)
+                    })
+                    .collect::<String>()
+            }),
+            &"\n".to_owned()
+        )
+    );
 }
 
 fn print_mark_indexes(maze: &Maze) {
@@ -101,19 +119,44 @@ fn print_mark_indexes(maze: &Maze) {
         for cell in line {
             match cell {
                 None => print!(" "),
-                Some(distance) => print!("{}", get_mark_index(distance)),
+                Some(mark_index) => print!("{}", MARKS[mark_index]),
             }
         }
         print!("\n");
     }
 }
 
-fn make_a_maze(mut state: State) -> Option<Maze> {
-    let self_distance = match state.maze.get_mut(state.position.x, state.position.y) {
-        Some(cell @ None) => cell,
-        _ => return None, // We're either out of bounds or the cell is already taken.
+fn finish_the_maze(maze: &mut Maze) {
+    for x in 0..MAZE_WIDTH {
+        for y in 0..MAZE_HEIGHT {
+            let None = maze.get(x, y).unwrap() else { continue; };
+            let mut current_distance = None;
+            let mut mark_indexes: Vec<_> = (0..MARKS.len()).collect();
+            mark_indexes.shuffle(&mut thread_rng());
+            for current_cell_mark_index in mark_indexes {
+                if adjacent(&maze, x, y).all(
+                    |(_adjacent_cell_positions, adjacent_cell_distance)| {
+                        // An adjacent cell is either not filled or we cannot go on it from the
+                        // current cell
+                        adjacent_cell_distance.map_or(true, |adjacent_cell_mark_index| {
+                            adjacent_cell_mark_index != get_next_mark_index(current_cell_mark_index)
+                        })
+                    },
+                ) {
+                    current_distance = Some(current_cell_mark_index);
+                    break;
+                }
+            }
+            *maze.get_mut(x, y).unwrap() = current_distance;
+        }
+    }
+}
+
+fn make_the_right_path(mut state: State) -> Option<Maze> {
+    let Some(self_distance @ None) = state.maze.get_mut(state.position.x, state.position.y) else {
+        return None; // We're either out of bounds or the cell is already taken.
     };
-    *self_distance = Some(state.distance);
+    *self_distance = Some(get_mark_index(state.distance));
     if state.position
         == (Position {
             x: MAZE_WIDTH - 1,
@@ -121,15 +164,12 @@ fn make_a_maze(mut state: State) -> Option<Maze> {
         })
     {
         if state.distance.0 >= DESIRED_DISTANCE {
-            for cell in state.maze.contents.iter_mut() {
-                
-            }
             return Some(state.maze);
         } else {
             return None;
         }
     }
-    for (_position, distance) in adjacent(&state.maze, state.position.x, state.position.y)
+    for (_position, mark_index) in adjacent(&state.maze, state.position.x, state.position.y)
         .filter_map(|cell| {
             if let (pos, Some(distance)) = cell {
                 Some((pos, distance))
@@ -138,7 +178,7 @@ fn make_a_maze(mut state: State) -> Option<Maze> {
             }
         })
     {
-        if get_mark_index(state.distance + 1) == get_mark_index(*distance) {
+        if get_mark_index(state.distance + 1) == *mark_index {
             return None; // One of the adjacent cells can be accessed from the current one.
         }
     }
@@ -152,7 +192,7 @@ fn make_a_maze(mut state: State) -> Option<Maze> {
                 position,
                 distance: state.distance + 1,
             };
-            if let maze @ Some(_) = make_a_maze(new_state) {
+            if let maze @ Some(_) = make_the_right_path(new_state) {
                 return maze;
             }
         }
@@ -161,7 +201,7 @@ fn make_a_maze(mut state: State) -> Option<Maze> {
             position: first_position,
             distance: state.distance + 1,
         };
-        if let maze @ Some(_) = make_a_maze(new_state) {
+        if let maze @ Some(_) = make_the_right_path(new_state) {
             return maze;
         }
     }
@@ -169,11 +209,21 @@ fn make_a_maze(mut state: State) -> Option<Maze> {
 }
 
 fn main() {
-    let maze = make_a_maze(State {
+    let mut maze = make_the_right_path(State {
         distance: Distance(0),
         position: Position { x: 0, y: 0 },
         maze: Matrix::new(None),
     })
     .expect("A maze of such size with such path length cannot be built!");
+    println!("Solution:");
     print_the_maze(&maze);
+    print!("\n\n");
+    finish_the_maze(&mut maze);
+    println!(
+        "Rules: get from {BEGINNING_MARK} to {END_MARK} by following the cells in this order: {}",
+        join(MARKS.iter(), ", ")
+    );
+    println!("{}", BEGINNING_MARK);
+    print_the_maze(&maze);
+    println!("{}", END_MARK);
 }
