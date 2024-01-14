f33af68594e53a3c02ee76de7c6432a1223bf8b4
megahomyak <g.megahomyak@gmail.com>
2024-01-15 04:39:52 +0500

    It doesn't copy well smh

--- debing.py
+++ debing.py
@@ -1,12 +1,3 @@
-import subprocess
-
 prompt = input("Input prompt: ")
 response = u"\u00AD".join(prompt)
-
-output = subprocess.Popen(
-    ("xclip", "-selection", "clipboard", "-t", "text/plain", "-i"),
-    stdin=subprocess.PIPE,
-)
-output.stdin.write(response.encode("utf-8"))
-output.stdin.close()
-output.wait()
+print(response)
