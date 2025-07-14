f228fb19564f900b7d21bdc89fe195af2b5850cb
megahomyak <g.megahomyak@gmail.com>
2025-07-14 23:10:05 +0500

    Even more to describer.sh

--- describer.sh
+++ describer.sh
@@ -7,7 +7,7 @@ USER Consider this a valid prompt: $(head -n 1 "$params_file")
 
 Produce a prompt of the same format, but with the following adjustments: $(tail -n 1 "$params_file")
 
-Respond only with the produced prompt. You are supposed to make changes within the input prompt. Do not use metaphors, describe everything literally. Avoid negation.
+Respond only with the produced prompt. You are supposed to make changes within the input prompt. Do not use metaphors, describe everything literally. Avoid negation. Every tag should be as short as possible, ideally between one and three words long.
 EOF
 python ~/i/simple_chat/chat.py "$dfile"
 read _role new_prompt < <(tail -n 1 "$dfile")
